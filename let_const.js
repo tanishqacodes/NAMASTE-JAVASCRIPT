05fc86243052bdc3acc9355488d5323e1a2cf582
tanishqacodes <tanishqasharma797@gmail.com>
2023-07-29 14:52:03 +0530

    let_const

---
+++ let_const.js
@@ -0,0 +1,12 @@
+console.log("b : ",b);
+// console.log(a); => we can't access let variable without declaration
+let a = 10;
+var b = 100;
+
+
+// let a => gives error
+// var a => not allowed
+
+console.log("a L ",this.a);
+
+console.log("b : ",this.b);
